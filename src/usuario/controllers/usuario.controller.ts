c688f51655166b204e0aed7f7fc82cd8a8f6943d
Mauricio Carvalho <mcarvalhojb@gmail.com>
2022-10-19 12:45:35 -0300

    added crud to usuario

--- src/usuario/controllers/usuario.controller.ts
+++ src/usuario/controllers/usuario.controller.ts
@@ -1,15 +1,49 @@
-import { Controller, Get, HttpCode, HttpStatus } from "@nestjs/common";
+import { Controller, Get, HttpCode, HttpStatus, Param, ParseIntPipe } from "@nestjs/common";
+import { Body, Delete, Post, Put } from "@nestjs/common/decorators";
 import { UsuarioService } from "src/usuario/services/usuario.service";
 import { Usuario } from "src/usuario/entities/usuario.entity";
 
-
-@Controller()
+@Controller('/usuario')
 export class UsuarioController {
-    constructor(private readonly usuarioService: UsuarioService) { }
 
-    @Get('/usuario')
-    @HttpCode(HttpStatus.OK)
-    async callFindAll(): Promise<Usuario[]> {
-        return await this.usuarioService.findAll()
+    constructor(
+        private readonly usuarioService: UsuarioService
+    ) { }
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    callCreate(@Body() usuario: Usuario): Promise<Usuario> {
+        return this.usuarioService.create(usuario);
     }
+
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    callFindAll(): Promise<Usuario[]> {
+        return this.usuarioService.findAll();
+    }
+
+    @Get('/:id')
+    @HttpCode(HttpStatus.OK)
+    callFindById(@Param('id', ParseIntPipe) id: number): Promise<Usuario> {
+        return this.usuarioService.findById(id);
+    }
+
+    @Get('/nome/:nome')
+    @HttpCode(HttpStatus.OK)
+    callFindByName(@Param('nome') nome: string): Promise<Usuario[]> {
+        return this.usuarioService.findByName(nome);
+    }
+
+    @Put()
+    @HttpCode(HttpStatus.OK)
+    callUpdate(@Body() usuario: Usuario): Promise<Usuario> {
+        return this.usuarioService.update(usuario);
+    }
+
+    @Delete('/:id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    callDelete(@Param('id', ParseIntPipe) id: number) {
+        return this.usuarioService.delete(id);
+    }
+
 }
--- src/usuario/services/usuario.service.ts
+++ src/usuario/services/usuario.service.ts
@@ -1,7 +1,8 @@
+import { HttpException, HttpStatus } from "@nestjs/common";
 import { Injectable } from "@nestjs/common/decorators";
 import { InjectRepository } from "@nestjs/typeorm";
+import { DeleteResult, ILike, Repository } from "typeorm";
 import { Usuario } from "src/usuario/entities/usuario.entity";
-import { Repository } from "typeorm";
 
 
 @Injectable()
@@ -11,8 +12,48 @@ export class UsuarioService {
         private usuarioRepository: Repository<Usuario>
     ) { };
 
-    async findAll(): Promise<Usuario[]> {
-        return await this.usuarioRepository.find()
+    async create(usuario: Usuario): Promise<Usuario> {
+        return await this.usuarioRepository.save(usuario);
+    }
 
+    async findAll(): Promise<Usuario[]> {
+        return await this.usuarioRepository.find();
+    }
+
+    async findById(id: number): Promise<Usuario> {
+        const buscaUsuario = await this.usuarioRepository.findOneBy({ id });
+
+        if (!buscaUsuario)
+            throw new HttpException('Usuário não encontrado!', HttpStatus.NOT_FOUND);
+
+        return buscaUsuario;
+    }
+
+    async findByName(nome: string): Promise<Usuario[]> {
+        return await this.usuarioRepository.find({
+            where: {
+                nome: ILike(`%${nome}%`)
+            }
+        });
+    }
+
+    async update(usuario: Usuario): Promise<Usuario> {
+
+        const buscaUsuario = await this.findById(usuario.id);
+
+        if (!buscaUsuario || !usuario.id)
+            throw new HttpException('Usuário não encontrado!', HttpStatus.NOT_FOUND);
+
+        return await this.usuarioRepository.save(usuario);
+    }
+
+    async delete(id: number): Promise<DeleteResult> {
+
+        let buscaUsuario = await this.findById(id);
+
+        if (!buscaUsuario)
+            throw new HttpException('Usuário não encontrado!', HttpStatus.NOT_FOUND);
+
+        return await this.usuarioRepository.delete(id);
     }
 }

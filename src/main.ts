0e19a30ec1ef4fe01598ccf82ce22b84a0bab2ca
WallaceArauj <wallace.hostile94@gmail.com>
2022-11-01 12:19:51 -0300

    feat: installed swagger and configured

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const config = new DocumentBuilder()
+  .setTitle('Motive')
+  .setDescription('Projeto Blog Pessoal')
+  .setContact("Motive","https://github.com/orgs/GrupoMotive/dashboard", "grupo1.generation.rj@email.com")
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
+
   process.env.TZ = '-03:00';
 
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(4000);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();

aad23db7d730a58a23b126f3562975ef384175de
WallaceArauj <wallace.hostile94@gmail.com>
2022-11-30 11:13:50 -0300

    feat: routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { Navbar } from './componentes/estaticos/navbar/Navbar';
+import { Aulas } from './paginas/aulas/Aulas';
+import { Contato } from './paginas/contato/Contato';
+import { Home } from './paginas/home/Home';
+import { Login } from './paginas/login/Login';
+import { Sobre } from './paginas/sobre/Sobre';
 
-function App() {
-  return (
-    
-  );
+function App(){
+  <>
+  <Router>
+  <Navbar />
+  <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/home' element={<Home />} />
+    <Route path='/aulas' element={<Aulas/>} />
+    <Route path='/contato' element={<Contato />}/>
+    <Route path='/sobre' element={<Sobre />} />
+    <Route path='/login' element={<Login />} />
+  </Routes>
+  </Router>
+  </>
 }
-
-export default App;

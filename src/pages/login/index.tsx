2c3e502e9cfc7b32d2d656ce3d36508bb8d9dc4d
igor02b1 <igor02b1@gmail.com>
2022-12-14 11:18:10 -0300

    feat: added new style to login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -74,31 +74,29 @@ function Login() {
 
   return (
     <Grid container direction='row' justifyContent='center' alignItems='center' sx={{ height: "100vh" }} className="imagem">
-
-
+      <Navbar/>
       <Grid xs={6}>
-
       </Grid>
       <Grid alignItems='center' xs={6}>
         <Box paddingX={20}>
-          <form onSubmit={handleSubmit(handleLogin)}>
-            <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>Entrar</Typography>
-            <TextField {...register('email')} id='email' label='email' variant='outlined' name='email' margin='normal' fullWidth />
-            <TextField {...register('senha')} id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
+        <form onSubmit={handleSubmit(handleLogin)} className="box-login">
+            <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' sx={{color: "#0DCA70", fontSize: "20px", fontFamily: ' rel="stylesheet"   href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"'}}>LOGIN</Typography>
+            <TextField {...register('email')} id='email' label='email' variant='filled' name='Email' margin='normal' color='success' fullWidth className='campo'/>
+            <TextField {...register('senha')} id='senha' label='senha' variant='filled' name='Senha' margin='normal' type='password' color='success' fullWidth className='campo' />
             <Box marginTop={2} textAlign='center'>
-              <Button type='submit' variant='contained' color='primary'>
-                Logar
+              <Button type='submit' variant='contained' color='primary' className='botão-login' sx={{backgroundColor: "#0DCA70", width: "251px", ":hover": { backgroundColor: "#000"} }}>
+                Login
               </Button>
+              <Box display='flex' justifyContent='center' marginTop={2}>
+                <Box marginRight={1} className='boxteste'>
+                  <Typography variant='subtitle1' gutterBottom align='center' className='txt3'>Não tem conta?</Typography>
+                </Box>
+                <Link to="/register">
+                  <Typography variant='subtitle1' gutterBottom align='center' className='textos2'>Cadastre-se</Typography>
+                </Link>
+              </Box>
             </Box>
           </form>
-          <Box display='flex' justifyContent='center' marginTop={2}>
-            <Box marginRight={1}>
-              <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
-            </Box>
-            <Link to="/register">
-              <Typography variant='subtitle1' gutterBottom align='center' className='textos1'>Cadastre-se</Typography>
-            </Link>
-          </Box>
         </Box>
       </Grid>
 

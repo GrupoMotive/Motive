c6d34494a75e9d062b1bdcc4466f9d8924ce7b1a
Mauricio Carvalho <mcarvalhojb@gmail.com>
2022-12-15 10:52:39 -0300

    feat: Improved footer visuals and it's components

--- src/components/static/footer/index.tsx
+++ src/components/static/footer/index.tsx
@@ -2,79 +2,94 @@ import React from "react";
 import Container from "@material-ui/core/Container";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import Link from "@material-ui/core/Link";
+import Typography from "@mui/material/Typography";
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import { Link } from "react-router-dom";
+import './footer.css'
 
 
-
-export default function Footer () {
+export default function Footer() {
   return <footer >
-    <Box 
-    px={{xs: 3, sm: 10}}
-    py={{xs: 5, sm: 10}}
-    bgcolor={"#040919"} 
-    color="white"
+    <Box
+      px={{ xs: 3, sm: 10 }}
+      py={{ xs: 5, sm: 10 }}
+      bgcolor={"#040919"}
+      color="white"
     >
       <Container maxWidth="lg">
         <Grid container spacing={5}>
           <Grid item xs={12} sm={4}>
-            <Box borderBottom={1}>Help</Box>
             <Box>
-              <Link href="/" color="inherit">
-                Contact
+              <img src="/WhiteLogo-Motive.png" alt="WhiteLogoMotive" height={50} width={200} />
+            </Box>
+            <Box pr={15}>
+              <Typography>
+                Somos a MOTIVE, site de saúde e bem-estar. {"\n"}
+              </Typography>
+              <Typography>
+                Proporcionamos uma qualidade de vida melhor, saudável e acessível para todas as idades.
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Box borderBottom={3} borderColor={"#0DCA70"} pb={1}>LINKS ÚTEIS</Box>
+            <Box pt={3} pb={1} borderBottom={2} borderColor={"#101522"}>
+              <Link to="/" className="LinkFooter" >
+                <ChevronRightIcon sx={{ color: "#0DCA70", fontSize: 30 }} />
+                <Typography sx={{ color: "white", ":hover": { opacity: 0.7} , transition: "0.2s all"}}>
+                  Contact
+                </Typography>
               </Link>
             </Box>
-            <Box>
-              <Link href="/" color="inherit">
-                Contact
+            <Box pt={1} pb={1} borderBottom={2} borderColor={"#101522"}>
+              <Link to='/' className="LinkFooter">
+                <ChevronRightIcon sx={{ color: "#0DCA70", fontSize: 30 }} />
+                <Typography sx={{ color: "white", ":hover": { opacity: 0.7} , transition: "0.2s all"}}>
+                  Contact
+                </Typography>
               </Link>
             </Box>
-            <Box>
-              <Link href="/" color="inherit">
-                Contact
+            <Box pt={1} pb={1} borderBottom={2} borderColor={"#101522"}>
+              <Link to='/' className="LinkFooter">
+                <ChevronRightIcon sx={{ color: "#0DCA70", fontSize: 30 }} />
+                <Typography sx={{ color: "white", ":hover": { opacity: 0.7} , transition: "0.2s all"}}>
+                  Contact
+                </Typography>
               </Link>
             </Box>
           </Grid>
           <Grid item xs={12} sm={4}>
-            <Box borderBottom={1}>Login</Box>
-            <Box>
-              <Link href="/" color="inherit">
-                Contact
+            <Box borderBottom={3} borderColor={"#0DCA70"} pb={1}>CONTATO</Box>
+            <Box pt={3} pb={1} borderBottom={2} borderColor={"#101522"}>
+              <Link to="/" className="LinkFooter" >
+                <ChevronRightIcon sx={{ color: "#0DCA70", fontSize: 30 }} />
+                <Typography sx={{ color: "white", ":hover": { opacity: 0.7} , transition: "0.2s all"}}>
+                  Contact
+                </Typography>
               </Link>
             </Box>
-            <Box>
-              <Link href="/" color="inherit">
-                Contact
+            <Box pt={1} pb={1} borderBottom={2} borderColor={"#101522"}>
+              <Link to='/' className="LinkFooter">
+                <ChevronRightIcon sx={{ color: "#0DCA70", fontSize: 30 }} />
+                <Typography sx={{ color: "white", ":hover": { opacity: 0.7} , transition: "0.2s all"}}>
+                  Contact
+                </Typography>
               </Link>
             </Box>
-            <Box>
-              <Link href="/" color="inherit">
-                Contact
-              </Link>
-            </Box>
-          </Grid>
-          <Grid item xs={12} sm={4}>
-            <Box borderBottom={1}>Sobre</Box>
-            <Box>
-              <Link href="/" color="inherit">
-                Contact
-              </Link>
-            </Box>
-            <Box>
-              <Link href="/" color="inherit">
-                Contact
-              </Link>
-            </Box>
-            <Box>
-              <Link href="/" color="inherit">
-                Contact
+            <Box pt={1} pb={1} borderBottom={2} borderColor={"#101522"}>
+              <Link to='/' className="LinkFooter">
+                <ChevronRightIcon sx={{ color: "#0DCA70", fontSize: 30 }} />
+                <Typography sx={{ color: "white", ":hover": { opacity: 0.7} , transition: "0.2s all"}}>
+                  Contact
+                </Typography>
               </Link>
             </Box>
           </Grid>
         </Grid>
-        <Box 
-        textAlign={"center"} 
-        pt={{xs: 5, sm: 10}} 
-        pb={{xs: 5, sm: 0}} >
+        <Box
+          textAlign={"center"}
+          pt={{ xs: 5, sm: 10 }}
+          pb={{ xs: 5, sm: 0 }} >
           Copyright <strong>Motive</strong> &reg; {new Date().getFullYear()}
         </Box>
       </Container>

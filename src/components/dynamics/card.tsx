110f743389c94df51f71dd7aeeb92d409bae943e
weslley matos <weslley_mattosibf@hotmail.com>
2022-12-15 17:13:02 -0300

    feat: added search option in classes page

--- src/components/dynamics/card.tsx
+++ src/components/dynamics/card.tsx
@@ -17,27 +17,28 @@ interface CardProps {
 export default function MediaCard({ titulo, local }: CardProps) {
   return (
     <Card className='card'>
-      <CardContent className='content'>
-        <Grid container justifyContent='space-between'>
-          <Grid item xs={3}>
-            <Typography sx={{fontWeight: "bold", marginLeft: "10px"}}gutterBottom variant="h5">
+      <CardContent className='content' sx={{ height: "35%" }}>
+        <Grid container sx={{ justifyContent: 'space-between', alignItems: "center" }}>
+          <Grid item xs={8}>
+            <Typography sx={{ fontWeight: "bold", fontSize: "20px", marginLeft: "10px" }} color="#000">
               {titulo}
             </Typography>
-            <Typography sx={{fontWeight: "bold", marginLeft: "10px"}} variant="body2" color="text.secondary">
+            <Typography sx={{ marginLeft: "10px", width: "100%", height: "50px" }} variant="body2" color="#000">
               {local}
             </Typography>
           </Grid>
-          <Grid item xs={3}>
+          <Grid item xs={4} >
             <CardActions>
               <Link to="/produto">
-              
-              <Button className='btn-comprar' sx={{
-                backgroundColor: "black",
-                color: "white",
-                width: "6rem",
-                fontFamily: 'monospace',
-                ":hover": {backgroundColor: "black", opacity: 0.8}
-              }}>Comprar</Button>
+
+                <Button className='btn-comprar' sx={{
+                  backgroundColor: "black",
+                  color: "white",
+                  width: "8rem",
+                  fontFamily: 'monospace',
+                  transition: "0.2s all",
+                  ":hover": { backgroundColor: "#030303" }
+                }}>Comprar</Button>
               </Link>
             </CardActions>
           </Grid>
--- src/pages/classes/index.tsx
+++ src/pages/classes/index.tsx
@@ -1,13 +1,70 @@
+
 import React, { useEffect, useState } from "react";
-import { Box, Divider, Grid, Typography } from "@mui/material";
+import { alpha, Box, Divider, Grid, InputAdornment, InputBase, styled, TextField, Typography } from "@mui/material";
+import IconButton from "@material-ui/core/IconButton";
 import Produtos from "../../models/produto";
 import api from "../../services/api";
 import MediaCard from "../../components/dynamics/card";
 import Footer from "../../components/static/footer";
 import Navbar from "../../components/static/navbar";
+import SearchIcon from '@mui/icons-material/Search';
+import { useForm } from 'react-hook-form'
+import './style.css'
+
+type FormValues = {
+  search: string
+}
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.6),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.7),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(3),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(TextField)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '600px',
+  },
+}));
 
 export default function Classes() {
   const [produtos, setProdutos] = useState<Produtos[]>([])
+  const [isSearch, setIsSerach] = useState(false);
+  const { register, handleSubmit } = useForm<FormValues>()
+
+
+  async function handleGetProducts(data: FormValues) {
+    if (data.search !== '') {
+      const res = await api.get(`produto/nome/${data.search}`)
+      setProdutos(res.data);
+    } else {
+      BuscaProduto()
+    }
+  }
 
   async function BuscaProduto() {
     const res = await api.get('produto');
@@ -16,93 +73,82 @@ export default function Classes() {
 
   useEffect(() => {
     BuscaProduto()
-  }, [produtos.length])
+  }, [])
 
   return (
     <Box>
-    <Box minHeight="55vh"
-    width="100%"
-    py={6}
-    px={{ xs: 2, lg: 0 }}
-    mx={-2} 
-    sx={{ 
-      backgroundImage: 'url(./bgaulas.jpg)',
-      backgroundSize: "cover",
-      backgroundPosition: "center",
-      display: "grid",
-      placeItems: "center",
-      }}>
-
-          <Typography variant="h2" sx={{
-          fontWeight: 'bold',
-          color: 'white',
-          paddingX: '50px'
-
-        }}> Aulas
-        
-          </Typography>
-
-      <Navbar />
-
-      <Grid container sx={{ width: '90%' }}>
-        {produtos.map(produto => (
-          <Grid item xs={4} sx={{ display: 'flex', justifyContent: 'center', marginY: "10px" }}>
-            <MediaCard titulo={produto.nome} local={produto.local} key={produto.id} />
-          </Grid>
-        ))}
-      </Grid>
-
-      <Box minHeight="25rem" alignItems="center" sx={{
-          p: 2,
-          mx: { xs: 2, lg: 3 },
-          mt: -8,
-          mb: 4,
-          boxShadow: 3,          
-          backgroundColor: "#fff",
+      <Box minHeight="55vh"
+        width="100%"
+        py={6}
+        sx={{
+          backgroundImage: 'url(./bgaulas.jpg)',
           backgroundSize: "cover",
           backgroundPosition: "center",
           display: "grid",
-          borderRadius: '16px',
-          
-      }}>
-
-        <Typography variant="subtitle1" sx={{
-          fontWeight: 'bold',
-          padding: '30px',
-        }}
-        >
-          <h2> Quem somos </h2>
-          <Divider color="#0DCA70" sx={{
-            height: 4, 
-            width: '100px',
-            borderRadius: '5px',
-          }} />
-
-
-        </Typography >
-
-        <Typography sx={{
-          paddingX: '30px'
+          placeItems: "center",
         }}>
 
-          Somos a MOTIVE, site de saúde e bem-estar.
+        < Navbar />
 
-          Pensando no conceito prevenção, recuperação da saúde e proporcionar uma qualidade de vida melhor, saudável e acessível, resolvemos fazer algo que pudesse ocupar a ociosidade e o tempo livre da população envolvendo desde crianças, adolescentes, jovens, adultos e principalmente idosos.
+        <form onSubmit={handleSubmit(handleGetProducts)}>
+          <TextField
+            {...register('search')}
+            label="Buscar Aulas"
+            color='green'
+            sx={{ backgroundColor: "gray", borderRadius: "8px", width: "600px" }}
+            variant="filled"
+            inputProps={{ style: { color: "#efeeee" } }}
+            InputProps={{
+              endAdornment: (
+                <IconButton>
+                  <SearchIcon />
+                </IconButton>
+              )
+            }}
+          />
+        </form>
 
-          O projeto reune um site com aulas presenciais em várias cidades do Rio de Janeiro, em parques e praias, praças e espaços de lazer. Reunimos atividades físicas e práticas corporais de forma coletiva ao ar livre, com um valor simbólico e com data e horário predefinido.
+        <Typography variant="h2" sx={{
+          fontWeight: 'bold',
+          color: 'white',
+          paddingX: '30px',
+          marginY: "30px"
 
-          Verdadeiros pontos de encontro com o bem-estar, para que possa se exercitar no seu tempo, no seu ritmo e ao ar livre. Então é só acessar o site, fazer seu cadastro, achar a aula ou o treino que mais combina com você e começar a se exercitar! Motive!
+
+        }}>
+          Aulas Disponíveis
+          <Divider color="#0DCA70" sx={{
+            height: 4,
+            width: '100px',
+            borderRadius: '5px',
+            position: "center",
+          }} />
 
         </Typography>
+      </Box>
+      <Box minHeight="25rem" alignItems="center" sx={{
+        p: 2,
+        mx: { xs: 2, lg: 3 },
+        mt: -8,
+        mb: 4,
+        boxShadow: 3,
+        backgroundColor: "#fff",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        display: "grid",
+        borderRadius: '16px',
+      }}>
 
-      
+        <Grid container spacing={4} sx={{ width: '100%', }}>
+          {produtos.map(produto => (
+            <Grid item xs={4} sx={{ marginY: "5px" }}>
+              <MediaCard titulo={produto.nome} local={produto.local} key={produto.id} />
+            </Grid>
+          ))}
+        </Grid>
 
+      </Box>
+      <Footer />
     </Box>
-  
-    < Footer />
-
-  </Box>
-  
-  </Box>
   )
 }

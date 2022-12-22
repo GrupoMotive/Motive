78cd7d4da4127eb145b459badc21a6e2a6b0ad2a
igor02b1 <igor02b1@gmail.com>
2022-12-22 11:48:28 -0300

    feat: added a new section to home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,8 +6,9 @@ import Navbar from "../../components/static/navbar";
 import './Home.css'
 import Footer from "../../components/static/footer";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from "react-responsive-carousel";
-import Weslley from '../../assets/images/Wesley.jpg'
+import capoeira from '../../assets/images/capoeira.jpg'
+import danca from '../../assets/images/danca.jpg'
+import yoga from '../../assets/images/yoga2.jpg'
 
 export default function Home() {
 
@@ -23,10 +24,16 @@ export default function Home() {
   }, [produtos.length])
 
   return (
+    <>
     <Box className="imagemHome">
       <Navbar />
       <Grid container xs={12} justifyContent='center' alignItems='center'  >
-        <Grid item xs={8} display='flex' flexDirection='column' justifyContent='center' className="animationfade">
+
+        <Grid item xs={8} display='flex' flexDirection='column' justifyContent='center' alignItems='center' className="animationfade"
+        sx={{
+          height: "calc(100vh - 80px)",
+
+        }}>
 
           <Typography sx={{ fontWeight: 'bold', fontSize: '65px', }} className='TituloHome' lineHeight='100%' align="center" >
             <br /> <br />  VOCÊ MAIS <span style={{ color: '#0DCA70', }}>SAUDÁVEL</span> <br /> COM ATIVIDADES AO AR LIVRE.
@@ -40,51 +47,24 @@ export default function Home() {
 
         </Grid>
 
-        <Box
-          display='flex'
-          justifyContent='center'
-          margin='100px'
-          padding='10px'
-          className="fundo"
-          borderRadius='20px'
-        >
-
-          <Carousel>
-
-            <div className="imageCarousel">
-              <img src={Weslley} alt="weslley" />
-              <p className="legend ">A vida em um constante movimento</p>
-            </div>
-            <div className="imageCarousel">
-              <img src={Weslley} alt="weslley" />
-              <p className="legend ">Viva a vida</p>
-            </div>
-            <div className="imageCarousel">
-              <img src={Weslley} alt="weslley" />
-              <p className="legend ">Tente não morrer vivendo</p>
-            </div>
-            <div className="imageCarousel">
-              <img src={Weslley} alt="weslley" />
-              <p className="legend ">Tente não morrer vivendo</p>
-            </div>
-            <div className="imageCarousel">
-              <img src={Weslley} alt="weslley" />
-              <p className="legend ">Tente não morrer vivendo</p>
-            </div>
-            <div className="imageCarousel">
-              <img src={Weslley} alt="weslley" />
-              <p className="legend ">Tente não morrer vivendo</p>
-            </div>
-          </Carousel>
-
-        </Box>
-
-      </Grid>
-
-      <Footer />
+        </Grid>
     </Box>
-
-
+    <Box className="box-cards">
+          <div className="div-cards">
+            <img src="https://i.ibb.co/mS7YCdV/yoga2.jpg" alt="dança" className="cards-home" />
+            <p className="legend ">A vida em um constante movimento</p>
+          </div>
+          <div className="div-cards">
+            <img src="https://i.ibb.co/ZGCDC00/dan-a.jpg" alt="capoeira" className="cards-home" />
+            <p className="legend ">Viva a vida</p>
+          </div>
+          <div className="div-cards">
+            <img src="https://i.ibb.co/Hq379Jj/capoeira.jpg" alt="yoga" className="cards-home"  />
+            <p className="legend ">Tente não morrer vivendo</p>
+          </div> 
+      </Box>
+    <Footer />
+    </>
   );
 
 

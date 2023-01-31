ebb14fe3853c5bd7f12d15d4774adfd7a6b2c3e8
weslley matos <weslley_mattosibf@hotmail.com>
2023-01-31 15:14:15 -0300

    updated backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,16 +11,16 @@ import { AppController } from './app.controller';
 
 @Module({
   imports: [
-    /*       TypeOrmModule.forRoot({
-            type: 'mysql',
-            host: 'localhost',
-            port: 3306,
-            username: 'root',
-            password: 'root',
-            database: 'db_motive',
-            entities: [Categoria, Usuario, Produto],
-            synchronize: true,
-          }), */
+    /* TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_motive',
+      entities: [Categoria, Usuario, Produto],
+      synchronize: true,
+    }), */
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DATABASE_URL,
